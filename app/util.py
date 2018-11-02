c04a1d1ba5ad898e9f6bca921e591a44b97dd328
anatolii2makaroff <anatolii2makaroff@gmail.com>
2018-11-02 11:00:15 +0000

    add suod

--- app/util.py
+++ app/util.py
@@ -4,7 +4,7 @@ from jinja2 import Template
 
 
 def run_shell(cmd):
-    return sp.check_output("{};exit 0".format(cmd), shell=True, stderr=sp.STDOUT).strip()
+    return sp.check_output("sudo {};exit 0".format(cmd), shell=True, stderr=sp.STDOUT).strip()
 
 
 def make_ha_config(vip, servers):
@@ -13,6 +13,7 @@ def make_ha_config(vip, servers):
     out = t.render(vip=vip, servers=servers)
     with open("/etc/haproxy/haproxy.cfg", "w+") as f:
         f.write(out)
+
 
 def do_service_stop0(name):
     res = run_shell("systemctl stop {} && echo \"ok\"".format(name))
