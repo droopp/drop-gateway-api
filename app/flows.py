a2f0047d4d9cbffc6fc4adab133edada98c6d74a
anatolii2makaroff <anatolii2makaroff@gmail.com>
2018-11-19 13:11:16 +0000

    add flow api with data

--- app/flows.py
+++ app/flows.py
@@ -2,6 +2,33 @@
 from drop_core import *
 
 
+@app.route("/api/v1.0/stat/flows",  methods=['POST'])
+@jwt_required()
+def flows2():
+    l = glob.glob(FLOWS_DIR + "/*.json")
+
+    data = []
+    for i in l:
+        with open(i) as f:
+            data.append(json.loads(f.read()))
+
+    res = "<root><flows>"
+
+    for row in data:
+        res += """<row>
+                    <name>{}</name>
+                    <active>{}</active>
+                    <version>{}</version>
+                    <priority>{}</priority>
+                    <entry>{}</entry>
+                    <data>{}</data>
+                 </row>""".format(row["name"], row["active"], row["version"],
+                                  row["priority"],
+                                  row["entry_ppool"],  json.dumps(row))
+
+    return res + '</flows></root>', 200
+
+
 @app.route("/api/v1/flows",  methods=['GET'])
 @jwt_required()
 def flows():
